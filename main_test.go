bd5ce9b526897eee1644518716a17232b296e701
Jorge Alberto Diaz Orozco (Akiel) <diazorozcoj@gmail.com>
2024-09-25 21:12:37 +0200

    Improve the regular expression so it can detect keys that are not in a new line

--- main_test.go
+++ main_test.go
@@ -28,6 +28,8 @@ ss://YWVzLTI1Ni1nY206ZmFCQW9ENTRrODdVSkc3@54.36.174.181:2376#Britain504%20%28t.m
 ss://YWVzLTI1Ni1nY206S2l4THZLendqZWtHMDBybQ==@ak1394.free.www.outline.network:8080#www.outline.network%20(japan)
 vmess://eyJhZGQiOiIxMTYuMjAzLjczLjM0IiwiYWlkIjoiMCIsImFscG4iOiIiLCJob3N0IjoiZ29vZ2xlLmNvbSIsImlkIjoiMmFhYzc3ZGUtYjNlNC00MDE3LTg0NWMtY2ExMzgwZjJlOGQwIiwibmV0Ijoid3MiLCJwYXRoIjoiL3RlbGVncmFtLWlkLUBwcml2YXRldnBucyIsInBvcnQiOiI4MCIsInBzIjoiMTExNyhAT3V0bGluZV9WcG4pIiwic2N5IjoiYXV0byIsInNuaSI6IiIsInRscyI6IiIsInR5cGUiOiIiLCJ2IjoiMiJ9
 vless://YWVzLTI1Ni1nY206ZmFCQW9ENTRrODdVSkc3@54.36.174.181:2376#Britain504%20%28t.me
+
+Страна: 🇳🇱 Нидерланды Срок действия: 3 дня Ключ: ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpUdTFSU2ZyakRoWGREaTlnWUYzeE9k@178.208.91.118:16942/?outline=1
 `
 	expectedServers := []string{
 		"ss://YWVzLTI1Ni1nY206WTZSOXBBdHZ4eHptR0M@54.36.174.181:5002",
@@ -37,9 +39,10 @@ vless://YWVzLTI1Ni1nY206ZmFCQW9ENTRrODdVSkc3@54.36.174.181:2376#Britain504%20%28
 		"ss://YWVzLTI1Ni1nY206ZmFCQW9ENTRrODdVSkc3@54.36.174.181:2376",
 		"ss://YWVzLTI1Ni1nY206UENubkg2U1FTbmZvUzI3QDUuMzkuNzAuMTM4OjgwOTA",
 		"ss://YWVzLTI1Ni1nY206S2l4THZLendqZWtHMDBybQ==@ak1394.free.www.outline.network:8080",
+		"ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpUdTFSU2ZyakRoWGREaTlnWUYzeE9k@178.208.91.118:16942",
 	}
 
 	servers := findServers(sample)
-	assert.Len(t, servers, 7)
+	assert.Len(t, servers, 8)
 	assert.EqualValues(t, expectedServers, servers)
 }
